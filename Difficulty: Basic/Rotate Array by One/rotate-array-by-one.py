ead474176105ced2cf47ef3d90465ff0c5d4923b
keethivasan R <keerthirohith02550@gmail.com>
2026-03-01 21:16:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+
+class Solution:
+    def rotate(self, arr):
+        l = 0 
+        r =len(arr)-1
+        
+        while l <= r:
+            arr[l],arr[r]=arr[r],arr[l]
+            l+=1
+            r-=1
+            
+        l = 1 
+        r =len(arr)-1
+        
+        while l <= r:
+            arr[l],arr[r]=arr[r],arr[l]
+            l+=1
+            r-=1
+        return arr
