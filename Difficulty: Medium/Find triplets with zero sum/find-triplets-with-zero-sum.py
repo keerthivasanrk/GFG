6c34f3fdc2488b77bbccab470a7ae35edfdf58b7
keethivasan R <keerthirohith02550@gmail.com>
2026-03-24 09:48:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find triplets with zero sum/find-triplets-with-zero-sum.py
+++ Difficulty: Medium/Find triplets with zero sum/find-triplets-with-zero-sum.py
@@ -2,17 +2,17 @@ class Solution:
     def findTriplets(self, arr):
         #code here
         arr.sort()
+        l=0
+        r=len(arr)-1
         for i in range(len(arr)-2):
-            l = i+1
-            r = len(arr)-1
-            
+            l=i+1
+            r=len(arr)-1
             while l<r:
                 s = arr[i]+arr[l]+arr[r]
-                if s==0:
+                if s == 0:
                     return True
-                    
-                elif s>0:
-                    r-=1
-                else:
+                elif s<0:
                     l+=1
-                        
+                else:
+                    r-=1
+        return False
