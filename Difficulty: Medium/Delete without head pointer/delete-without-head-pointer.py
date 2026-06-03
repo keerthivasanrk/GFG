e9febc9f644a9879b16e48dc483f1faa15b84306
keethivasan R <keerthirohith02550@gmail.com>
2026-06-03 06:56:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Delete without head pointer/delete-without-head-pointer.py
@@ -0,0 +1,14 @@
+''' Structure of linked list Node
+	{
+		class Node:
+		    def __init__(self, data):  
+		        self.data = data
+		        self.next = None
+	}
+'''
+class Solution:
+    def deleteNode(self, del_node):
+        # code here
+        del_node.data = del_node.next.data
+        del_node.next = del_node.next.next
+        return
