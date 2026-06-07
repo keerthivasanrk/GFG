8aa98a9eedde30adaf1f16a9f68dd1c235926f46
keethivasan R <keerthirohith02550@gmail.com>
2026-06-07 07:45:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.py
@@ -0,0 +1,22 @@
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def detectLoop(self, head):
+        # code here
+        if not head :
+            return False
+            
+        s=f=head
+        
+        while f and f.next:
+            s = s.next
+            f = f.next.next
+            if s==f:
+                return True
+
+        return False
