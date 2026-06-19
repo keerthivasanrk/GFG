b50cab822effb9b88a0cd92a4dabac54e7864c3e
keethivasan R <keerthirohith02550@gmail.com>
2026-06-19 08:44:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.py
@@ -0,0 +1,24 @@
+''' Structure of linked list Node
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+'''
+def removeDuplicates(head):
+    #code here
+    if not head:
+        return
+    
+    curr = head
+    
+    while curr and curr.next:
+        
+        if curr.next.data == curr.data :
+            curr.next = curr.next.next
+        else:
+            curr = curr.next
+        
+    return head
