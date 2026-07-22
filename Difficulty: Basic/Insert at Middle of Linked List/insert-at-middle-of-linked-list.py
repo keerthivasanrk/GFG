15632ffe2253e0a3692517aa21048448f481e488
keethivasan R <keerthirohith02550@gmail.com>
2026-07-22 11:51:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Insert at Middle of Linked List/insert-at-middle-of-linked-list.py
+++ Difficulty: Basic/Insert at Middle of Linked List/insert-at-middle-of-linked-list.py
@@ -10,19 +10,15 @@ class Node:
 class Solution:
     def insertInMiddle(self, head, x):
         #code here
-        if not head :
+        s=f=head
+        if not head:
             return Node(x)
-            
-        f = s  = head
+        
         while f.next and f.next.next:
-            s = s.next
+            s=s.next
             f = f.next.next
-        
-        nn = Node(x)
-        
-        nn.next = s.next
-        s.next = nn
+        k = s.next    
+        s.next = Node(x)
+        s.next.next = k
         
         return head
-        
-        
