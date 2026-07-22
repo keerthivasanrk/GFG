b153e0b4310d503d949dbdb3f6de127619c52e22
keethivasan R <keerthirohith02550@gmail.com>
2026-07-22 18:43:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.py
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.py
@@ -8,29 +8,24 @@ class Node:
 class Solution:
     def sortedMerge(self, head1, head2):
         # code here
-        curr1 = head1
-        curr2 = head2
-        dummy = Node(0)
-        gh = Node(0)
-        gh.next = dummy
-        while curr1 and curr2:
-            if curr1.data>=curr2.data:
-                
-                dummy.next = curr2
-                dummy = dummy.next
-                curr2 = curr2.next
+        dum = Node(0)
+        d = dum
+        h1 = head1
+        h2 = head2
+        while h1 and h2:
+            if h1.data<=h2.data:
+                d.next = h1
+                h1 = h1.next
                 
             else:
-                dummy.next = curr1
-                dummy = dummy.next
-                curr1 = curr1.next
-        while curr1 :
-            dummy.next = curr1
-            dummy = dummy.next
-            curr1 = curr1.next
-        while curr2:
-            dummy.next = curr2
-            dummy = dummy.next
-            curr2 = curr2.next
-            
-        return gh.next.next
+                d.next = h2
+                h2 = h2.next
+                
+            d = d.next
+        if h1:
+            d.next = h1
+        if h2:
+            d.next = h2
+        
+        return dum.next
+                
