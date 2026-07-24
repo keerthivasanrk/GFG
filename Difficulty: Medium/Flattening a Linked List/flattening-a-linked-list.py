2dd9f1bd6a402fb894572c3c803cce842901d72b
keethivasan R <keerthirohith02550@gmail.com>
2026-07-24 09:48:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.py
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.py
@@ -8,35 +8,54 @@ class Node:
 '''
 
 class Solution:
-   
-        # code here
-         # 1. Helper function to merge two sorted bottom-linked lists
-    def merge(self, a, b):
-        if not a: return b
-        if not b: return a
-        
-        # Compare data and link via the bottom pointer
-        if a.data < b.data:
-            result = a
-            result.bottom = self.merge(a.bottom, b)
-        else:
-            result = b
-            result.bottom = self.merge(a, b.bottom)
+    
+    
+    
+    def m(self,l1,l2):
+        if not l1: 
+            return l2
+        if not l2:
+            return l1
             
-        # Ensure the next pointer is cleared as it's flattened
-        result.next = None 
-        return result
+            
+        dum = Node(0)
+        d = dum
+        while l1 and l2:
+            if l1.data<l2.data:
+                d.bottom = l1
+                l1=l1.bottom
+                
+            else:
+                d.bottom = l2
+                l2=l2.bottom
+                
+            d = d.bottom
+            d.next = None
+        if l1:
+            d.bottom = l1
+            
+        if l2:
+            d.bottom = l2
+            
+        return dum.bottom
+            
         
-    # 2. Main function to flatten the list
+    
     def flatten(self, root):
-        # Base case: if list is empty or has only one node
         if not root or not root.next:
             return root
             
-        # Recurse for the list on the right
         root.next = self.flatten(root.next)
-        
-        # Merge the current node's list with the flattened right list
-        root = self.merge(root, root.next)
+        root =  self.m(root,root.next)
         
         return root
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
